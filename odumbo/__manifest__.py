e7859e6e6fdde5345a131502be8aa70994bb29e5
augusto-weiss <awe@adhoc.com.ar>
2025-10-01 14:18:17 +0000

    [ADD] odumbo: app store

---
+++ odumbo/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Integración Mercado Libre",
+    'summary': """
+        Sincroniza tus Precios, Stock y Ventas de Mercado Libre vía API, sin modificar tu ​Odoo.
+    """,
+    'description': """
+        Sincroniza tus Precios, Stock y Ventas de Mercado Libre vía API, sin modificar tu ​Odoo.
+    """,
+    'author': 'Adhoc S.A.',
+    'website': 'https://odumbo.com/',
+    'version': "16.0.1.0.0",
+    'images':  ['static/description/portada.png'],
+    'depends': [],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,
+    'license': 'AGPL-3',
+}
